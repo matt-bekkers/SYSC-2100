fd08deca813362cc54a14dc8a2fa487705ab57cd
Matthé Bekkers <matthedeb@hotmail.com>
2025-01-05 19:01:35 -0500

    Blank files

---
+++ Labs/Lab1/concordance.py
@@ -0,0 +1,34 @@
+"""
+SYSC 2100 Winter 2024
+Lab 1, Part 3, Exercise 4, Extra-Practice Exercise 5
+"""
+
+__author__ = ''
+__student_number__ = ''
+
+import string
+
+# For information about the string module, type help(string) at the shell
+# prompt, or browse "The Python Standard Library", Section "Built-in Types",
+# Subsection "Text Sequence Type - str" in the documentation
+# (available @ python.org).
+
+
+def build_concordance(filename: str) -> dict[str, list[int]]:
+    """Return a concordance of words in the text file
+    with the specified filename.
+
+    The concordance is stored in a dictionary. The keys are the words in the
+    text file. The value associated with each key is a list containing the line
+    numbers of all the lines in the file in which the word occurs.)
+
+    >>> concordance = build_concordance('sons_of_martha.txt')
+    """
+
+
+# Extra-Practice: Exercise 5 Solution
+
+
+if __name__ == '__main__':
+    pass
+    # Write your solution to Extra-practice Exercise 5 here
